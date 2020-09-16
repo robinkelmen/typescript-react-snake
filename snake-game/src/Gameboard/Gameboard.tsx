a6eacf8ddb18028a28ed5e16905f91a9f59baf0a
Robin Kelmen <robin.kelmen@my.wheaton.edu>
2020-09-16 01:52:24 -0500

    Started making game board

--- snake-game/src/Gameboard/Gameboard.tsx
+++ snake-game/src/Gameboard/Gameboard.tsx
@@ -1,15 +1,48 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './Gameboard.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./Gameboard.css";
 
+const XPOS_HEAD_START: number = 15;
+const YPOS_HEAD_START: number = 15;
+const XPOS_APPLE_START: number = Math.floor(Math.random() * Math.floor(30));
+const YPOS_APPLE_START: number = Math.floor(Math.random() * Math.floor(30));
 
-
-
-
-
-export default class Gameboard extends Component{
-
-
-
-
+interface Props {
+  nodes: [];
 }
+
+export default class Gameboard extends Component<Props> {
+  play() {}
+
+  render() {
+    return (
+      <div className="Grid">
+        <button onClick={() => this.play()}> Start Playing</button>
+      </div>
+    );
+  }
+}
+
+const createGrid = (xSize: number, ySize: number) => {
+  const nodes = [];
+  for (let row = 0; row < xSize; row++) {
+    const currentcol = [];
+    for (let col = 0; col < ySize; col++) {
+      currentcol.push(createNode(row, col));
+    }
+
+    nodes.push(currentcol);
+  }
+  return nodes;
+};
+
+const createNode = (row: number, col: number) => {
+  return {
+    row,
+    col,
+    isHead: row === XPOS_HEAD_START && col === YPOS_HEAD_START,
+    isBody: false,
+    isApple: row === XPOS_APPLE_START && col === YPOS_APPLE_START,
+    previousNode: null,
+  };
+};
--- snake-game/src/Gameboard/Snakebody/Snakenode.tsx
+++ snake-game/src/Gameboard/Snakebody/Snakenode.tsx
@@ -1,31 +1,34 @@
-import React, { Component } from 'react';
-import './Snakenode.css';
+import React, { Component } from "react";
+import "./Snakenode.css";
 
-
-
-
-interface Props{
-    xPos : number;
-    yPos: number; 
-    isHead: boolean; 
-    isBody: boolean;
-    isApple: boolean;
-    previousNode?: Snakenode;
+interface Props {
+  xPos: number;
+  yPos: number;
+  isHead: boolean;
+  isBody: boolean;
+  isApple: boolean;
+  previousNode?: Snakenode;
 }
 
 export default class Snakenode extends Component<Props> {
-
-
-
-render(){
-    const {xPos, yPos, isHead, isBody, isApple }  = this.props; 
+  handleClick() {
+    console.log("[" + this.props.xPos + "," + this.props.yPos + "]");
+  }
+  render() {
+    const { xPos, yPos, isHead, isBody, isApple } = this.props;
+    const className = isHead
+      ? "node_head"
+      : isApple
+      ? "node_apple"
+      : isBody
+      ? "node_body"
+      : "";
     return (
-       <div id= {`node-${xPos}-${yPos}`}
-       
-       
-       > foo </div>);
-    
-}
-
-
+      <div
+        id={`node-${xPos}-${yPos}`}
+        className={`node ${className}`}
+        onClick={() => this.handleClick()}
+      ></div>
+    );
+  }
 }
